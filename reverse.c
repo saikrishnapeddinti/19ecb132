693ce63c236dffc6e53df03a816e65e9dde2ab2f
saikrishna peddinti <101582842+saikrishnapeddinti@users.noreply.github.com>
2022-05-31 17:38:22 +0000

    added output

--- reverse.c
+++ reverse.c
@@ -11,3 +11,28 @@
         printf("%d\n", *ptr--);  
   return 0;  
 }  
+
+
+Enter 10 integer numbers
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0
+
+Elements of array in reverse order ...
+0
+9
+8
+7
+6
+5
+4
+3
+2
+1

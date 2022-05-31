98f8816a3e7ad8e00f3328107bf2b2186a9a5af5
saikrishna peddinti <101582842+saikrishnapeddinti@users.noreply.github.com>
2022-05-31 17:03:32 +0000

    output added

--- doublelinkedlists.c
+++ doublelinkedlists.c
@@ -241,3 +241,21 @@ void search()
         }  
     }          
 }  
+
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+7
